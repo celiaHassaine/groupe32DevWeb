4bb7ece16883d48b4290a1e579eaa26ec9b25dca
Namurois Thomas <56117432+NamuroisThomas@users.noreply.github.com>
2020-04-22 12:46:28 +0200

    Ajout sandwich

---
+++ src/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\Documents\GitHub\groupe32DevWeb\src\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
