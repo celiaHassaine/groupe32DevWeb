43576e862102f8e4e6c2916ee088d5631c70b685
Florent Theys <f.theys@students.ephec.be>
2020-05-21 00:00:05 +0200

    amélioration produit admin

--- src/produits/admin.py
+++ src/produits/admin.py
@@ -3,8 +3,14 @@ from .models import Attribut, Valeur, Produit, Commande, ProduitAttribut, Catego
 
 # Register your models here.
 
+
+class ProduitAdmin(admin.ModelAdmin):
+    fields = ('nom', 'prix', 'categorie', 'en_vente', 'description', 'image')
+    list_display = ('nom', 'categorie', 'en_vente', 'prix')
+
+
 admin.site.register(Categorie)
-admin.site.register(Produit)
+admin.site.register(Produit, ProduitAdmin)
 admin.site.register(Attribut)
 admin.site.register(Valeur)
 admin.site.register(Commande)
