ff269bb5c2f38a1ada940ad1f487c544a722c607
Florent Theys <f.theys@students.ephec.be>
2020-05-20 22:59:04 +0200

    modification structure DB

--- src/produits/admin.py
+++ src/produits/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from .models import Attribut, Valeur, Produit, Commandes, ProdAttr, Categorie
+from .models import Attribut, Valeur, Produit, Commande, ProduitAttribut, Categorie
 
 # Register your models here.
 
+admin.site.register(Categorie)
+admin.site.register(Produit)
 admin.site.register(Attribut)
 admin.site.register(Valeur)
-admin.site.register(Produit)
-admin.site.register(Commandes)
-admin.site.register(ProdAttr)
-admin.site.register(Categorie)
-
+admin.site.register(Commande)
