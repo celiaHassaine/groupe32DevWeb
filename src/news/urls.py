325072ae7afa5dbdec28c94637a7a887f60e3bc0
Florent Theys <f.theys@students.ephec.be>
2020-05-24 01:58:16 +0200

    CSS: ajout carousel, suppression du code d'essai de l'apps new

--- src/news/urls.py
+++ src/news/urls.py
@@ -1,11 +1,7 @@
-from django.conf.urls import url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.accueil, name="Page d'accueil"),
-    path('liste/', views.listing, name='listing'),
-    path('noimage/', views.listingsansimg, name='listingsansimg'),
-    url(r'^search/$', views.search),
 ]
--- src/news/views.py
+++ src/news/views.py
@@ -1,55 +1,14 @@
-import requests
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
 from .models import News
-from rest_framework import status
-from rest_framework.reverse import reverse as api_reverse
-from django.contrib.auth import get_user_model
+from django.apps import apps
 
-from rest_framework_jwt.settings import api_settings
-
-
-# Create your views here.
-
-def listing(request):
-    news = News.objects.all()
-    formated_news = [
-        "<li><img src=\"{}\" alt=\"{}\"><br/>{}<br/>{}</li>".format(album.img.url, album.titre, album.titre,
-                                                                    album.contenu) for album in news]
-    message = """<ul>{}</ul>""".format("\n".join(formated_news))
-    return HttpResponse(message)
-
-
-def listingsansimg(request):
-    news = News.objects.all()
-    formated_news = ["<li>{}<br/>{}</li>".format(album.titre, album.contenu) for album in news]
-    message = """<ul>{}</ul>""".format("\n".join(formated_news))
-    return HttpResponse(message)
-
-
-def search(request):
-    query = request.GET.get('query')
-    if not query:
-        albums = News.objects.all()
-    else:
-        # title contains the query is and query is not sensitive to case.
-        albums = News.objects.filter(titre__icontains=query)
-
-    if not albums.exists():
-        message = "Misère de misère, nous n'avons trouvé aucun résultat !"
-    else:
-        albums = ["<li><img src=\"{}\" alt=\"{}\"><br/>{}<br/>{}</li>".format(album.img.url, album.titre, album.titre,
-                                                                              album.contenu) for album in albums]
-        message = """
-            Nous avons trouvé les articles correspondant à votre requête ! Les voici :
-            <ul>{}</ul>
-        """.format("</li><li>".join(albums))
-
-    return HttpResponse(message)
+Categorie = apps.get_model('produits', 'Categorie')
 
 
 def accueil(request):
+    categories = Categorie.objects.all()
     actualites = News.objects.order_by('-id')[:3]
     return render(request, 'accueil.html', {
-        'actualites' : actualites,
+        'actualites': actualites,
+        'categories': categories,
     })
